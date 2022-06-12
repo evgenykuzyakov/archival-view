fc774277244411ad396bc5f512d748c053410551
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-06-12 10:44:25 -0700

    Add BRRR price

--- src/fetchers/refNear.js
+++ src/fetchers/refNear.js
@@ -3,21 +3,29 @@ import Big from "big.js";
 
 Big.DP = 40;
 
-export const Title = "REF/wNEAR";
-const RefPoolId = 79;
+export const Title = "REF prices";
+const RefPoolIds = {
+  "REF/wNEAR": 79,
+  "BRRR/wNEAR": 3474,
+};
 const RefFinanceContractId = "v2.ref-finance.near";
 
 export async function computeValueForBlochHeight(viewCall) {
-  const refPool = keysToCamel(
-    await viewCall(RefFinanceContractId, "get_pool", {
-      pool_id: RefPoolId,
+  const promises = await Promise.all(
+    Object.entries(RefPoolIds).map(async ([key, poolId]) => {
+      const refPool = keysToCamel(
+        await viewCall(RefFinanceContractId, "get_pool", {
+          pool_id: poolId,
+        })
+      );
+      const refPrice = Big(refPool.amounts[1])
+        .div(Big(refPool.amounts[0]))
+        .div(1e6);
+
+      return {
+        [key]: refPrice.toFixed(6),
+      };
     })
   );
-  const refPrice = Big(refPool.amounts[1])
-    .div(Big(refPool.amounts[0]))
-    .div(1e6);
-
-  return {
-    "REF/wNEAR": refPrice.toFixed(6),
-  };
+  return Object.assign({}, ...promises);
 }
